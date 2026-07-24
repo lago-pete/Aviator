ea4020931fb6fbf5be54a26c94ab5b497f572453
lago-pete <lagonegropeter@gmail.com>
2026-07-24 14:43:19 -0700

    Pulling Correct Satellite Info

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,38 @@
 #include <Wire.h>
 #include <vector>
 
-
-
 void setup()
 {
   Serial.begin(115200);
-  Serial2.begin(38400,SERIAL_8N1, 16,17); // Testing this baud first because datasheets have been all over the place.
+  Serial2.begin(38400, SERIAL_8N1, 16, 17); // Testing this baud first because datasheets have been all over the place.
   Serial.println("Connecting....");
-  
 }
 
 void loop()
 {
-  if(Serial2.available() > 0){
+  while (Serial2.available() > 0)
+  {
     int ans = Serial2.read();
-    Serial.write(ans);
+    if (ans == '$')
+    {
+      String sentence = "$";
+      while (ans != '\n')
+      {
+        if (Serial2.available() > 0)
+        {
+          ans = Serial2.read();
+          if(ans != '\n'){
+            sentence += (char)ans;
+          } 
+        }
+      }
+  
+      int index = sentence.indexOf(',');
+      String check = sentence.substring(0,index);
+      if(check == "$GNGGA"){
+        Serial.println(sentence);
+      }
+      
+    }
   }
-
-
 }
-
-
