860bbc88b3805795c93cc9c7b91031a7813c3556
lago-pete <lagonegropeter@gmail.com>
2026-08-03 17:37:01 -0700

    ESP32 Officially sending Laptop communication over Iphone Hotspot

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,40 @@
 #include <Wire.h>
 #include <WiFi.h>
 
+IPAddress laptopIP(172, 20, 10, 3);
+
+WiFiUDP udp;
 
 
-void setup(){
+
+void setup()
+{
   Serial.begin(115200);
-  WiFi.begin("Pete's", "Redsox34");
+  WiFi.begin("Pete Phone", "Redsox34");
+  udp.begin(3434); 
 
-  while (WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED)
+  {
     Serial.print(".");
     delay(1000);
     Serial.println(WiFi.status());
   }
-        
 
   Serial.println("connected!");
   Serial.println(WiFi.localIP());
+
+  
 }
 
 void loop()
 {
   
+
+  udp.beginPacket(laptopIP, 4269); 
+  udp.write((const uint8_t *)"hello", 5);
+  udp.endPacket();
+
+  delay(2000);
 }
+
+//Check WireShark
